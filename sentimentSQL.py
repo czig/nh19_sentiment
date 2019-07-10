03603751ec9bced70d7981fb19cf3670877e4ff7
Caleb <caleb.ziegler@gmail.com>
2019-07-10 11:44:08 -0500

    Change sentimentSQL to output multiple databases for different pages/filtering

--- sentimentSQL.py
+++ sentimentSQL.py
@@ -11,9 +11,13 @@ import matplotlib.pyplot as plt
 #initialize sentiment analysis
 sid = SentimentIntensityAnalyzer()
 
-#initialize sql database
+#initialize input sql database
 engine = create_engine('sqlite:///./nh19_fb.db')
-out_engine = create_engine('sqlite:///./nh19_sentiment.db')
+
+#initialize output sql databases
+raw_engine = create_engine('sqlite:///./raw_sentiment.db')
+nh19_engine = create_engine('sqlite:///./nh19_sentiment.db')
+guy_engine = create_engine('sqlite:///./guy_sentiment.db')
 
 #pull data using sql query
 df = pd.read_sql("""select comm.*,
@@ -39,6 +43,9 @@ for index, row in df.iterrows():
 	df.at[index,'compound'] = sentiment['analysis']['compound']
 	sentiments.append(sentiment)
 
+#output raw sentiment to database
+df.to_sql('CommentSentiment',con=raw_engine,if_exists="replace")
+
 #make different datasets for each page
 embassy_df = df[df['page'] == 'USEmbassyGeorgetown']
 southcom_df = df[df['page'] == 'southcom']
@@ -46,37 +53,23 @@ nh_df = df[df['page'] == 'AFSOUTHNewHorizons']
 afsouth_df = df[df['page'] == 'AFSouthern']
 guyana_df =df[(df['page'] != 'USEmbassyGeorgetown')& (df['page'] != 'southcom') & (df['page'] != 'AFSOUTHNewHorizons') & (df['page'] != 'AFSouthern')]
 
+#output guyana dataframe to database
+guyana_df.to_sql('CommentSentiment',con=guy_engine,if_exists="replace")
+
 #filter embassy and southcom comments for new horizons
 embassyNh_df = embassy_df[(embassy_df['post_message'].str.contains("NH19|New Horizons|NewHorizons|Military",case=False,na=False)) | (embassy_df['message'].str.contains("NH19|New Horizons|NewHorizons|Military|USA|United States",case=False,na=False))]
 southcomNh_df = southcom_df[(southcom_df['post_message'].str.contains("NH19|New Horizons|NewHorizons|Guyana",case=False,na=False)) | (southcom_df['message'].str.contains("NH19|New Horizons|NewHorizons|Guyana",case=False,na=False))]
+afsouthNh_df = afsouth_df[(afsouth_df['post_message'].str.contains("NH19|New Horizons|NewHorizons|Guyana",case=False,na=False)) | (afsouth_df['message'].str.contains("NH19|New Horizons|NewHorizons|Guyana",case=False,na=False))]
 
-filtered_df = pd.concat([embassyNh_df,southcomNh_df,nh_df, afsouth_df, guyana_df])
-#filtered_df['neg'] = 0.0
-#filtered_df['neu'] = 0.0
-#filtered_df['pos'] = 0.0
-#filtered_df['compound'] = 0.0
+filtered_df = pd.concat([embassyNh_df, southcomNh_df, nh_df, afsouthNh_df])
 
-#sentiments = []
-#for index,row in filtered_df.iterrows():
-#    sentiment = {}
-#    sentiment['comment'] = row['message']
-#    sentiment['post'] = row['post_message']
-#    sentiment['analysis'] = sid.polarity_scores(row['message'])
-#    filtered_df.at[index,'pos'] = sentiment['analysis']['pos'] 
-#    filtered_df.at[index,'neg'] = sentiment['analysis']['neg'] 
-#    filtered_df.at[index,'neu'] = sentiment['analysis']['neu'] 
-#    filtered_df.at[index,'compound'] = sentiment['analysis']['compound'] 
-#    sentiments.append(sentiment)
+print('embassy: ',embassyNh_df.shape)
+print('southcom: ',southcomNh_df.shape)
+print('newHorizons: ',nh_df.shape)
+print('afsouth: ',afsouthNh_df.shape)
+print('total US related: ',filtered_df.shape)
+print('total Guyana: ',guyana_df.shape)
+print('all raw records: ',df.shape)
 
-
-print('embassy',len(embassyNh_df.index))
-print('southcom',len(southcomNh_df.index))
-print('newHorizons',len(nh_df.index))
-print('total',len(filtered_df.index))
-
-page_stats = filtered_df.groupby("page")
-print(page_stats.describe())
-print(page_stats.mean())
-
-filtered_df.to_sql('CommentSentiment',con=out_engine,if_exists="replace")
-filtered_df.to_excel('Output.xlsx')
+#output NH and guyana dataframes to database
+filtered_df.to_sql('CommentSentiment',con=nh19_engine,if_exists="replace")
