c6084ba3d7fd296dba38ea7227bb4510111151fa
Caleb <caleb.ziegler@gmail.com>
2019-07-10 10:11:26 -0500

    Update doeTopicModeling script for third doe trial

--- doeTopicModeling.sh
+++ doeTopicModeling.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
-#TOPICS=(5 100)
-#CHUNKSIZE=(100 4000)
-#PASSES=(10 100)
-#UPDATE_EVERY=(0 1)
+TOPICS=(29 77)
+CHUNKSIZE=(2050 4000)
+UPDATE_EVERY=(0 3000)
 
-#for topic in "${TOPICS[@]}"; do
-    #for chunk in "${CHUNKSIZE[@]}"; do
-        #for pass in "${PASSES[@]}"; do
-            #for update in "${UPDATE_EVERY[@]}"; do
-            #done
-        #done
-    #done
-#done
+for topic in "${TOPICS[@]}"; do
+    for chunk in "${CHUNKSIZE[@]}"; do
+        for update in "${UPDATE_EVERY[@]}"; do
+            python ./topicModeling.py --type posts --date 2019-04-01 --num_topics ${topic} --chunk_size ${chunk} --num_passes 10 --update_every ${update} --iterations 50 --name doe3 --ignore --logs
+        done
+    done
+done
 
 #hardcode since for loop would run too many
-python ./topicModeling.py --type posts --date 2019-04-01 --num_topics 5 --chunk_size 2050 --num_passes 55 --update_every 3001 --iterations 50 --name doe2 --ignore --logs
-python ./topicModeling.py --type posts --date 2019-04-01 --num_topics 100 --chunk_size 2050 --num_passes 55 --update_every 3001 --iterations 50 --name doe2 --ignore --logs
-python ./topicModeling.py --type posts --date 2019-04-01 --num_topics 53 --chunk_size 100 --num_passes 55 --update_every 3001 --iterations 50 --name doe2 --ignore --logs
-python ./topicModeling.py --type posts --date 2019-04-01 --num_topics 53 --chunk_size 4000 --num_passes 55 --update_every 3001 --iterations 50 --name doe2 --ignore --logs
-python ./topicModeling.py --type posts --date 2019-04-01 --num_topics 53 --chunk_size 2050 --num_passes 10 --update_every 3001 --iterations 50 --name doe2 --ignore --logs
-python ./topicModeling.py --type posts --date 2019-04-01 --num_topics 53 --chunk_size 2050 --num_passes 100 --update_every 3001 --iterations 50 --name doe2 --ignore --logs
-python ./topicModeling.py --type posts --date 2019-04-01 --num_topics 53 --chunk_size 2050 --num_passes 55 --update_every 1 --iterations 50 --name doe2 --ignore --logs
-python ./topicModeling.py --type posts --date 2019-04-01 --num_topics 53 --chunk_size 2050 --num_passes 55 --update_every 0 --iterations 50 --name doe2 --ignore --logs
-python ./topicModeling.py --type posts --date 2019-04-01 --num_topics 53 --chunk_size 2050 --num_passes 55 --update_every 3001 --iterations 50 --name doe2 --ignore --logs
-python ./topicModeling.py --type posts --date 2019-04-01 --num_topics 53 --chunk_size 2050 --num_passes 55 --update_every 3001 --iterations 50 --name doe2 --ignore --logs
-python ./topicModeling.py --type posts --date 2019-04-01 --num_topics 53 --chunk_size 2050 --num_passes 55 --update_every 3001 --iterations 50 --name doe2 --ignore --logs
-python ./topicModeling.py --type posts --date 2019-04-01 --num_topics 53 --chunk_size 2050 --num_passes 55 --update_every 3001 --iterations 50 --name doe2 --ignore --logs
+python ./topicModeling.py --type posts --date 2019-04-01 --num_topics 29 --chunk_size 3025 --num_passes 10 --update_every 4500 --iterations 50 --name doe3 --ignore --logs
+python ./topicModeling.py --type posts --date 2019-04-01 --num_topics 77 --chunk_size 3025 --num_passes 10 --update_every 4500 --iterations 50 --name doe3 --ignore --logs
+python ./topicModeling.py --type posts --date 2019-04-01 --num_topics 53 --chunk_size 3025 --num_passes 10 --update_every 3000 --iterations 50 --name doe3 --ignore --logs
+python ./topicModeling.py --type posts --date 2019-04-01 --num_topics 53 --chunk_size 3025 --num_passes 10 --update_every 0 --iterations 50 --name doe3 --ignore --logs
+python ./topicModeling.py --type posts --date 2019-04-01 --num_topics 53 --chunk_size 2050 --num_passes 10 --update_every 4500 --iterations 50 --name doe3 --ignore --logs
+python ./topicModeling.py --type posts --date 2019-04-01 --num_topics 53 --chunk_size 4000 --num_passes 10 --update_every 4500 --iterations 50 --name doe3 --ignore --logs
+python ./topicModeling.py --type posts --date 2019-04-01 --num_topics 53 --chunk_size 3025 --num_passes 10 --update_every 4500 --iterations 50 --name doe3 --ignore --logs
+python ./topicModeling.py --type posts --date 2019-04-01 --num_topics 53 --chunk_size 3025 --num_passes 10 --update_every 4500 --iterations 50 --name doe3 --ignore --logs
+python ./topicModeling.py --type posts --date 2019-04-01 --num_topics 53 --chunk_size 3025 --num_passes 10 --update_every 4500 --iterations 50 --name doe3 --ignore --logs
